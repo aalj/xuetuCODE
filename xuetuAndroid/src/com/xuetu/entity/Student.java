563e8046061a9e6f213b8ab82681f16ff57b9e38
woshikangyi <taoyannia1>
2016-03-15 22:40:29 +0800

    问题的显示页面 web端提交问题的servlet getSubjectBySubId    测试OK getSchoolBySch_id    测试OK
    listView的中的点击

--- xuetuAndroid/src/com/xuetu/entity/Student.java
+++ xuetuAndroid/src/com/xuetu/entity/Student.java
@@ -6,12 +6,14 @@
  *
  *   ver     date      		author
  * ──────────────────────────────────
- *   		 2016年1月29日 		Stone
+ *   		 2016年3月15日 		Stone
  *
  * Copyright (c) 2016, TNT All Rights Reserved.
 */
 
 package com.xuetu.entity;
+
+import java.sql.Date;
 
 /**
  * ClassName:Student <br/>
@@ -20,7 +22,7 @@ package com.xuetu.entity;
  * @author Stone
  * @version
  * @since Ver 1.1
- * @Date 2016年1月29日 上午11:15:10
+ * @Date 2016年3月15日 上午18:18:10
  *
  * @see
  * 
@@ -43,7 +45,7 @@ public class Student {
 	private int stuPoints = 0;
 	private School school = null;
 	private String stuPwd = null;
-
+	private Date Stu_create_date = null;
 	public Student() {
 
 		//
@@ -52,7 +54,7 @@ public class Student {
 
 	public Student(int stuId, String stuName, String stuEmail, String stuPhone, String stuIma, String stuSex,
 			int stuAge, String stuUgrade, String stuMajor, String stuSigner, int stuPoints, School school,
-			String stuPwd) {
+			String stuPwd,Date Stu_create_date) {
 		super();
 		this.stuId = stuId;
 		this.stuName = stuName;
@@ -67,6 +69,7 @@ public class Student {
 		this.stuPoints = stuPoints;
 		this.school = school;
 		this.stuPwd = stuPwd;
+		this.Stu_create_date = getStu_create_date();
 	}
 
 	/**
@@ -110,6 +113,14 @@ public class Student {
 	 */
 	public String getStuName() {
 		return stuName;
+	}
+
+	public Date getStu_create_date() {
+		return Stu_create_date;
+	}
+
+	public void setStu_create_date(Date stu_create_date) {
+		Stu_create_date = stu_create_date;
 	}
 
 	/**
---
+++ xuetuAndroid/src/com/xuetu/entity/Subject.java
@@ -0,0 +1,58 @@
+/**
+ * Subject.java
+ * com.xuetu.entity
+ *
+ * Function： TODO 
+ *
+ *   ver     date      		author
+ * ──────────────────────────────────
+ *   		 2016年3月6日 		Stone
+ *
+ * Copyright (c) 2016, TNT All Rights Reserved.
+*/
+
+package com.xuetu.entity;
+/**
+ * ClassName:Subject
+ * Function: 学科类型
+ * Reason:	 TODO ADD REASON
+ *
+ * @author   Stone
+ * @version  
+ * @since    Ver 1.1
+ * @Date	 2016年3月6日		上午12:19:59
+ *
+ * @see 	 
+
+ */
+public class Subject {
+	
+	public int getSubId() {
+		return SubId;
+	}
+	public Subject() {
+
+		// TODO Auto-generated constructor stub
+
+	}
+	public Subject(int subId, String name) {
+		super();
+		SubId = subId;
+		this.name = name;
+	}
+	public void setSubId(int subId) {
+		SubId = subId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	int SubId = 0;
+			String name= null ;
+	
+	
+
+}
+
