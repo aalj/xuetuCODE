484d3f3a89ae24c27fb9a3174c9be39c02da4a20
BBB <704678803@QQ.COM>
2016-03-16 09:06:27 +0800

    2016.3.16早上

--- xuetuAndroid/src/com/xuetu/ui/LoginActivity.java
+++ xuetuAndroid/src/com/xuetu/ui/LoginActivity.java
@@ -12,7 +12,6 @@ import com.lidroid.xutils.http.RequestParams;
 import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
-import com.xuetu.ForgetPwdActivity;
 import com.xuetu.R;
 import com.xuetu.entity.Student;
 import com.xuetu.utils.GetHttp;
