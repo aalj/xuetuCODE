2e31f65908f627606a3798e14ac959b06f4441e5
woshikangyi <taoyannia1>
2016-03-16 09:03:28 +0800

    显示问题和提交问题的servlet

---
+++ xuetuWeb/src/com/xuetu/web/android/GetPageQuestion.java
@@ -0,0 +1,63 @@
+package com.xuetu.web.android;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.xuetu.dao.QuestionIml;
+import com.xuetu.entity.Question;
+import com.xuetu.service.QuestionService;
+import com.xuetu.service.inter.QuestionServiceInter;
+
+/**
+ * Servlet implementation class GetAllQuestion
+ */
+@WebServlet("/GetPageQuestion")
+public class GetPageQuestion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	QuestionServiceInter q = new QuestionService(new QuestionIml());
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		
+		List<Question> questions = new ArrayList<Question>();
+		questions = q.queryLimitQuestion(1, 4);
+		System.out.println(questions.get(0).getQuesText());
+		String jsonStr = null;
+		Gson gson = new GsonBuilder()  
+				  .setDateFormat("yyyy-MM-dd HH:mm:ss")  
+				  .create();
+		jsonStr = gson.toJson(questions);
+		PrintWriter pw = response.getWriter();
+		pw.write(jsonStr);
+		
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=UTF-8");
+		doGet(request, response);
+	}
+
+}
