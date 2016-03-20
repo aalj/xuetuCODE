e5466e7f6fd4e190270dff867075a5b353164371
BBB <704678803@QQ.COM>
2016-03-20 23:31:38 +0800

    2016.3.20晚上同步

--- xuetuAndroid/src/com/xuetu/ui/YouHuiJuanInfomationActivity.java
+++ xuetuAndroid/src/com/xuetu/ui/YouHuiJuanInfomationActivity.java
@@ -45,8 +45,8 @@ public class YouHuiJuanInfomationActivity extends Activity {
 		zhekoudejutixingxi = (TextView) findViewById(R.id.zhekoudejutixingxi);
 		shiyongdidianshuoming = (TextView) findViewById(R.id.shiyongdidianshuoming);
 		head_CircleImageView = (CircleImageView) findViewById(R.id.head_CircleImageView);
-		backToDecember=(ImageView) findViewById(R.id.imageView_backToDecember);
-		getDatasAndsetDates() ;
+		backToDecember = (ImageView) findViewById(R.id.imageView_backToDecember);
+		getDatasAndsetDates();
 	}
 
 	public void getDatasAndsetDates() {
@@ -55,6 +55,9 @@ public class YouHuiJuanInfomationActivity extends Activity {
 		coupon = mycoupon.getCoupon();
 		BitmapUtils bitmapUtils = new BitmapUtils(this);
 		bitmapUtils.display(head, GetHttp.getHttpBCL() + coupon.getStoreName().getStoImg());
+		BitmapUtils bitmapUtils1 = new BitmapUtils(this);
+		bitmapUtils1.display(head_CircleImageView, GetHttp.getHttpBCL() + coupon.getStoreName().getStoImg());
+
 		youhuijuanxingxi.setText(coupon.getCouInfo());
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日HH时mm分ss秒");
 		String time_format = dateFormat.format(coupon.getConValidity());
@@ -65,30 +68,24 @@ public class YouHuiJuanInfomationActivity extends Activity {
 				+ "吃，如果有来生，要做一棵树， 站成永恒， 没有悲欢的姿势。一半在土里安详， 一半在风里飞扬， 一半洒落阴凉， 一半沐浴阳光。 非常沉默，非常骄傲， 从不依靠，从不寻找。");
 
 	}
-	
-	
-	public void onclick(View v){
+
+	public void onclick(View v) {
 		switch (v.getId()) {
 		case R.id.imageView_backToDecember:
 			Intent intent = new Intent();
-			intent.setClass(this,TheCollectionOfYouHuiJuanActivity.class);
+			intent.setClass(this, TheCollectionOfYouHuiJuanActivity.class);
 			startActivity(intent);
 			break;
-			/**跳转到店家**/
+		/** 跳转到店家 **/
 		case R.id.head_CircleImageView:
 			Intent intent1 = new Intent();
-//			intent1.setClass(this,StoneNameActivity.class);
+			// intent1.setClass(this,StoneNameActivity.class);
 			startActivity(intent1);
 			break;
 		default:
 			break;
 		}
-		
-		
-		
-		
+
 	}
-	
-	
 
 }
