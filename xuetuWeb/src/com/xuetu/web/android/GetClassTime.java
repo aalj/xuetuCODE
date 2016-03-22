c1d6dd101ff0ea5829e9fd1a434e25d1af76c2f6
602847305 <602847305@qq.com>
2016-03-22 21:09:00 +0800

    ///////********////////////////////

--- xuetuWeb/src/com/xuetu/web/android/GetClassTime.java
+++ xuetuWeb/src/com/xuetu/web/android/GetClassTime.java
@@ -1,6 +1,7 @@
 package com.xuetu.web.android;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,7 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.xuetu.dao.CLassTimeDao;
+import com.xuetu.entity.MyClass;
 import com.xuetu.service.ClassTimeService;
 import com.xuetu.service.inter.ClassTimeServiceInter;
 
@@ -28,26 +32,37 @@ public class GetClassTime extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.setCharacterEncoding("utf-8");
 		request.setCharacterEncoding("utf-8");
-		
-		int stu_id=Integer.parseInt(request.getParameter("stu_id"));//从安卓端传过来的学生id
-		System.out.println("stu_id>>>>>>>>>>>>>>>>>>>>>>>>>"+stu_id);  
-		CLassTimeDao classtimedao = new CLassTimeDao();
+		System.out.println("stu_id>>>>>>>>>>>>111>>>>>>>>>>>>>");  
+		String sstu_id=request.getParameter("stu_id");//从安卓端传过来的学生id
+		CLassTimeDao classtimedao = new CLassTimeDao();           //新建一个CLassTimeDao 对象
+		System.out.println("stu_id>>>>>>>>>>>>222>>>>>>>>>>>>>");  
+		//将安卓端传送来的学生id  查找对应的学生的所选课程,然后将这些课程id装到集合里面
+		int stu_id= Integer.parseInt(sstu_id);
+		System.out.println("stu_id>>>>>>>>>>>>333>>>>>>>>>>>>>");
 		List<Integer> classID = classtimedao.get_student_clsID(stu_id);
-//		int [] classid ={};
+		System.out.println("stu_id>>>>>>>>>>>>444>>>>>>>>>>>>>");
+		//获得符合现在时间的课程,(条件-- 星期几,第几节课,)得出符合现在时间段课程的所有课程对象
+		List<MyClass> myclasslist = classtimedao.getMyClass();
+		System.out.println("stu_id>>>>>>>>>>>>555>>>>>>>>>>>>>");
+		//将两个所得出的值中的 classID进行匹配,匹配到相同的则 表示有对应的课程,并且符合  提前5分钟以内,并且迟到不超过10分钟
+		boolean b = classtimedao.isStudy(classID, myclasslist);
+		long time_ss = 0;
 		
-//		int ii[] = null ;
-//		
-//		for(int i=0;i<classID.size();i++)
-//		{
-//			ii[i]=classID.get(i);
-//		}
-//		
-		if(classID.get(0)!= null)
+		//全部匹配成功后则,如果b的值为true 获取time_ss的值,否则值为0;
+		if(b)
 		{
-			for(int i=0;i<classID.size();i++)
-			classtimedao.getClass();
+			time_ss  = classtimedao.sendTime_SS(b);
 		}
+		System.out.println("sssssssssssssssss"+time_ss);
+		PrintWriter pw = response.getWriter();
+		Gson gson = new GsonBuilder().create();
+//		int  intgson = 0;
+		String jsonStr = null;
 		
+		jsonStr = gson.toJson(time_ss);
+		
+		pw.write(jsonStr);
+		pw.close();
 		
 	}
 
