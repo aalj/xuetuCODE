8c677306cbba42a0b40548b10443b62eed3e9642
aalj <dqliangjun@126.com>
2016-10-04 10:46:47 +0800

    重构整个项目结构

---
+++ xuetuAS/app/src/main/java/com/xuetu/constant/BaseApi.java
@@ -0,0 +1,18 @@
+package com.xuetu.constant;
+
+/**
+ * Created by liang on 2016/10/4.
+ */
+
+public class BaseApi {
+    /**
+     * 服务器地址
+     */
+    public static String urllj = "http://192.168.0.12:8080/xuetuWeb/";
+//    public static String urllj = "http://www.bestsnail.com:8965/xuetuWeb/";
+    public static String urlbcl = "http://www.bestsnail.com:8965/xuetuWeb/";
+    public static String urllc = "http://www.bestsnail.com:8965/xuetuWeb/";
+    public static String urlky = "http://www.bestsnail.com:8965/xuetuWeb/";
+
+
+}
---
+++ xuetuAS/app/src/main/java/com/xuetu/utils/GetHttp.java
@@ -0,0 +1,60 @@
+/**
+ * GetHttp.java
+ * com.xuetu.utils
+ *
+ *
+ *   ver     date      		author
+ * ──────────────────────────────────
+ *   		 2016年3月12日 		liang
+ *
+ * Copyright (c) 2016, TNT All Rights Reserved.
+*/
+
+package com.xuetu.utils;
+
+import com.xuetu.constant.BaseApi;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author liang
+ * @version
+ * @since Ver 1.1
+ * @Date 2016年3月12日 下午2:48:26
+ *
+ * @see
+ * 
+ */
+public class GetHttp {
+	
+
+	public static String getHttpLJ() {
+
+
+			String property = BaseApi.urllj;
+			return property;
+
+
+	}
+	public static String getHttpBCL() {
+		String property = BaseApi.urllj;
+		return property;
+		
+	}
+	public static String getHttpLC() {
+		String property = BaseApi.urllj;
+		return property;
+		
+	}
+	public static String getHttpKY() {
+		String property = BaseApi.urllj;
+		return property;
+		
+	}
+
+}
